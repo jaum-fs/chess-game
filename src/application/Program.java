4c1d23c41abda0bc6f95917da32b75655b0a6b02
jaum-fs <martins_joao@id.uff.br>
2022-08-18 23:26:36 -0300

    primeiras classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		ChessMatch x = new ChessMatch();
+		UI.printBoard(x.getPieces());
 	}
 
 }
