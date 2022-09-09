7743ca930d79357a54a272e57f96a7c2228efbea
jaum-fs <martins_joao@id.uff.br>
2022-09-09 09:50:35 -0300

    movimentos possíveis

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -15,18 +17,30 @@ public class Program {
 		
 		
 		while(true) {
-			UI.printBoard(x.getPieces());
-			System.out.println();
-			System.out.println("Posição de origem: ");
-			ChessPosition source = UI.readChessPosition(sc);
+			try {
+				UI.clearScreen();
+				
+				UI.printBoard(x.getPieces());
+				System.out.println();
+				System.out.println("Source position: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.println("Target position: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = x.performChessMove(source,target);
+			}
+			catch (ChessException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 			
-			System.out.println();
-			System.out.println("Posição de destino: ");
-			ChessPosition target = UI.readChessPosition(sc);
 			
-			ChessPiece capturedPiece = x.performChessMove(source,target);
 		}
-
 	}
-
 }
